7ae26e1f6dd476f899908e7b1aab4e4be1c0fe25
misstitank3 <tianna.m.mccoy@gmail.com>
2025-05-22 13:07:33 -0700

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import { LanguageProvider } from '@/lib/il8n/provider';
 import './globals.css';
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: 'ICE Tea Watch',
@@ -12,6 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="bg-white text-black dark:bg-zinc-900 dark:text-white">
+        <Analytics />
         <LanguageProvider>
           <main className="max-w-4xl mx-auto p-4">{children}</main>
         </LanguageProvider>
