be755cbfe5991757aea950c20afe7c091fe8caa6
misstitank3 <tianna.m.mccoy@gmail.com>
2025-06-06 13:56:25 -0700

    adding logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,19 @@ import KnowYourRights from '@/components/KnowYourRights';
 import LanguageSwitcher from '@/components/language-toggle/LToggle';
 import ICEArrestQuestions from '@/components/Questions';
 import { useTranslations } from '@/lib/il8n/useTranslations';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
   const { t } = useTranslations();
   return (
     <div className="flex flex-col gap-4 w-full">
+      <div className="w-full m-auto flex flex-col justify-center text-center items-center">
+        <Image src="/logo.svg" alt="ICE Tea Watch Logo" width={400} height={40} className="mb-10" />
+        <h1 className="text-4xl font-bold">{t('homeTitle')}</h1>
+        <p className="text-lg">{t('privacyTagline')}</p>
+      </div>
       <LanguageSwitcher />
-      <h1 className="text-4xl font-bold">{t('homeTitle')}</h1>
-      <p className="text-lg">{t('privacyTagline')}</p>
       <div className="flex justify-center gap-4 mt-8 flex-col">
         <Link href="https://ko-fi.com/techwitch" className="px-6 py-2 border rounded-xl" target="_blank">
           {t('supportProject')}
